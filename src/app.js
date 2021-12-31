cbd2d2dcb4d706ac5fe68aca2cbefe43fc375738
InaSLew <ina.shenlewander@gmail.com>
2021-12-31 15:00:06 +0100

    Project cards can be toggled

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
+// Hamburger menu reference and its callabck function
 const hamburgerBtn = document.getElementById('hamburger');
 const navList = document.getElementById("nav-list");
-
 const toggleBtn = () => navList.classList.toggle('show');
 
+// Project card detail
+const targetCards = document.getElementsByClassName('project-card');
+const toggleCardDetail = (index, element) => {
+    const targetCard = document.getElementsByClassName('project-card')[index];
+    const targetDetail = targetCard.getElementsByClassName('project-card-detail')[0];
+    targetCard.classList.toggle('expand');
+    targetDetail.classList.toggle('show');
+}
+
+// Adding EventListeners
 hamburgerBtn.addEventListener('click', toggleBtn);
+
+for (let index = 0; index < targetCards.length; index++) {
+    const element = targetCards[index];
+    element.addEventListener('click', () => toggleCardDetail(index, element));
+}
