769d2695aad89d4a501ac5db43af074f51a4b398
InaSLew <ina.shenlewander@gmail.com>
2022-01-13 01:24:07 +0100

    Added HeroArea component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import './Header.css';
+import HeroArea from './HeroArea/HeroArea';
 import NavBar from './NavBar/NavBar';
 
 const Header = () => {
     return (
         <header className="hero">
             <NavBar />
+            <HeroArea />
         </header>
     );
 }
