4e34ffcaa9094f0d4e2f5a5b6baf7222b10fc888
InaSLew <ina.shenlewander@gmail.com>
2022-01-13 01:15:27 +0100

    Moved <a> css rule into NavBar.css

--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import './NavBar.css';
 import avatar from '../avatar.png';
 
+// Note: on refactor list
+
 const NavBar = () => {
     return (
         <div className="nav">
             <img className="logo" src={avatar} alt="Ina Shen-Lewander's avatar"/>
-            <ul id="nav-list">
+            <ul id="nav-list">    
                 <li><a href="#about-me">About me</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact-me">Contact</a></li>
