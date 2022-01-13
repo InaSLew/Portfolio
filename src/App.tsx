595902bc6b9055e5aa9d18d04581d38e1b01da3a
InaSLew <ina.shenlewander@gmail.com>
2022-01-13 01:50:41 +0100

    Section component basically done

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div>
       <Header />
-      <Section />
-      <Section />
+      <Section id = {'about-me'} />
+      <Section id = {'projects'} />
       <Footer />
     </div>
   );
